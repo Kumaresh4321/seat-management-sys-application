ceb62894170574e7cefd2fe514540bb509f344da
Mridula Muralidharam <19z332@psgtech.ac.in>
2022-05-10 12:50:44 +0530

    added employee dashboard app and page, need to add request page for employee

--- seat_mng_sys/urls.py
+++ seat_mng_sys/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('', include('employeedashboard.urls'))
+    path('', include('employeedashboard.urls')),
+    path('', include('pages.urls')),
+    path('', include('admindashboard.urls'))
 ]
