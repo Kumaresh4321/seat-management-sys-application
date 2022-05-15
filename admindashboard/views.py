15307018e2242c770181a10c05272f83ac2e270a
Mridula Muralidharam <19z332@psgtech.ac.in>
2022-05-15 09:39:26 +0530

    logout for admin

--- admindashboard/views.py
+++ admindashboard/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from employeedashboard.views import displayemployee
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
 def dashboard(request):
     context = displayemployee(request, request.user)
     return render(request, 'admin_dash.html', context)
-
+    
+@login_required
 def requests(request):
     context = displayemployee(request, request.user)
     return render(request, 'allot_req.html', context)
--- employeedashboard/views.py
+++ employeedashboard/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
 from .models import Employee
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
 def emphome(request):
     context = displayemployee(request, request.user)
     return render(request, 'employee_dash.html', context)
 
+@login_required
 def empreq(request):
     context = displayemployee(request, request.user)
     return render(request, 'realloc_emp.html', context)
 
+@login_required
 def displayemployee(request, user):
     emp_user = user.username
     print(emp_user)
