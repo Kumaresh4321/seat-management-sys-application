3dc4b467d08bc424022fe520241b783fbdccc1aa
Kumaresh <19z327@psgtech.ac.in>
2022-05-15 18:05:49 +0530

    Tables done

--- seats/models.py
+++ seats/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models import Value
+    
 
-# Create your models here.
+class Seat(models.Model):
+    seat_number = models.IntegerField()
+    floor_number = models.CharField(max_length=6, choices=(('1','One'),
+    ('2', 'Two')))
+    tower_id = models.CharField(max_length=6, choices=(('A', 'Tower A'),('B', 'Tower B')))
+    state = models.CharField(max_length=6, choices=(
+        ('oc','Occupied'),
+        ('un', 'Unoccupied'),
+        ('bu', 'Buffer'),
+        ), default='Unoccupied')
+    shiftid = models.CharField(max_length=6, choices=(
+        ('ap', 'APAC'),
+        ('uk',  'UK'),
+    ), default='UK')
+    auto_increment_id = models.AutoField(primary_key=True)
+    
+    
