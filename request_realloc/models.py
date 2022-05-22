307debb83eca4ad6b85b1303dd331d7c1eb85f7a
Sravya-V-2000 <19z348@psgtech.ac.in>
2022-05-22 11:36:03 +0530

    Form details add to model
    All form info is taken and added to requests model

--- request_realloc/models.py
+++ request_realloc/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class rel_request(models.Model):
+    request_id =models.CharField(max_length=50, unique=True)
+    lh_name=models.CharField(max_length=50)
+    lh_id=models.CharField(max_length=50)
+    dh_name=models.CharField(max_length=50)
+    dh_id=models.CharField(max_length=50)
+    reason=models.CharField(blank=True,max_length=500)
+    status=models.CharField(max_length=10, default="Pending")
--- request_realloc/views.py
+++ request_realloc/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import requestForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def add_req(request):
+    submitted = False
+    if request.method == "POST":
+        form = requestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('add_req?submitted=True')
+
+    else:
+        form = requestForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'realloc_emp.html',{'form':form, 'submitted':submitted})
